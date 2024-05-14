333497664e0cbd6aaa9b98633d70e6112c8289d3
atiks88 <141978852+atiks88@users.noreply.github.com>
2024-05-14 07:19:58 +0000

    commit but not satisfied

--- src/components/App.js
+++ src/components/App.js
@@ -4,28 +4,35 @@ import './../styles/App.css';
 import { useState } from "react/cjs/react.production.min";
 
 const App = () => {
+  const [inputValue, setInputValue] = useState(""); 
+  const [tasks, setTasks] = useState([]);
 
-  const [inputValue, setInputValue] = useState("");
-  const [tasks, setTasks] = useState([]); 
-
-  let createTask = (e) =>{
-
+  const createTask = () => {
     setTasks([...tasks, inputValue]);
     setInputValue("");
+  }
+  const DeleteBtn =(index)=>{
+    const newTasks = [...tasks];
+    newTasks.splice(index, 1);
+    setTasks(newTasks);
   }
 
   return (
     <div>
-        <label>To-Do List</label>
-        <input 
+      <label>To-Do List</label>
+      <input 
         type="text" 
-        value={inputValue} // Set input value to state value
-        onChange={(e) => setInputValue(e.target.value)} // Update state with input value
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
       /> 
-        <button onClick={createTask}>Add Todo</button>
-        <ul>{tasks.map((task, index) => (<li key={index}>{task}</li>))}</ul>
+      <button onClick={createTask}>Add Todo</button>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>{task}<button onClick={() => DeleteBtn(index)}>Delete</button></li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
 export default App
