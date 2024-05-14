0faa7b16054c948824b35e338faef8c62cfb7773
atiks88 <141978852+atiks88@users.noreply.github.com>
2024-05-14 06:20:59 +0000

    final Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,29 @@
 
 import React from "react";
 import './../styles/App.css';
+import { useState } from "react/cjs/react.production.min";
 
 const App = () => {
+
+  const [inputValue, setInputValue] = useState("");
+  const [tasks, setTasks] = useState([]); 
+
+  let createTask = (e) =>{
+
+    setTasks([...tasks, inputValue]);
+    setInputValue("");
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+        <label>To-Do List</label>
+        <input 
+        type="text" 
+        value={inputValue} // Set input value to state value
+        onChange={(e) => setInputValue(e.target.value)} // Update state with input value
+      /> 
+        <button onClick={createTask}>Add Todo</button>
+        <ul>{tasks.map((task, index) => (<li key={index}>{task}</li>))}</ul>
     </div>
   )
 }
